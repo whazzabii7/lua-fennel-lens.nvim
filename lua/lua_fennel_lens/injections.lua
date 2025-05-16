bd565f628a75d737635c46cf2ffdf659ba0d64e2
whazzabii7 <whazzabii7@gmail.com>
2025-05-16 23:05:47 +0200

    updated

--- lua/lua_fennel_lens/injections.lua
+++ lua/lua_fennel_lens/injections.lua
@@ -3,20 +3,22 @@ local M = {}
 
 function M.setup()
   local fennel_injection = [[
-    (
-      (function_call
-        name: (identifier) @fennel_fn (#eq? @fennel_fn "eval")
-        arguments: (arguments
-          (string) @fennel_code
-        )
-      )
+    ((function_call
+      name: [
+        (identifier) @_fennel_identifier
+        (_
+          _
+          (identifier) @_fennel_identifier)
+      ]
+      arguments: (arguments
+        (string
+          content: _ @injection.content)))
       (#set! injection.language "fennel")
-      (#set! injection.combined)
-    )
+      (#eq? @_fennel_identifier "eval"))
   ]]
 
-  local _, ts_query = pcall(require, "vim.treesitter.query")
-  if ts_query then
+  local ok, ts_query = pcall(require, "vim.treesitter.query")
+  if ok and ts_query then
     ts_query.set("lua", "injections", fennel_injection)
   end
 end
