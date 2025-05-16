2b0296a90efb0a8ec4adcc4e478d99905c4723ef
whazzabii7 <whazzabii7@gmail.com>
2025-05-16 22:25:00 +0200

    added

--- lua/lua_fennel_lens/init.lua
+++ lua/lua_fennel_lens/init.lua
@@ -2,8 +2,8 @@
 local M = {}
 
 function M.setup()
-  require "lua_fennel_lens.injections".setup()
-  require "lua_fennel_lens.lsp_bridge".setup()
+  require "lua.lua_fennel_lens.injections".setup()
+  require "lua.lua_fennel_lens.lsp_bridge".setup()
 end
 
 return M
